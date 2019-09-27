07969a408220980f675813b97b1412a5b45425f4
Maiker <m2ikr23@gmail.com>
2019-09-26 20:54:39 -0400

    cambios en el form

--- src/js/popup.js
+++ src/js/popup.js
@@ -4,6 +4,10 @@ import { showError } from "./popup/message_helpers";
 import { renderTimeEntries, renderProjectTimerButtons } from "./popup/render_helper";
 import { loadAllTimeEntries, loadAllProjectsAndActivities } from './popup/api_call_helper';
 
+$('#create_task').click(function () {
+  $('#form_create_task').attr("style","");
+});
+
 chrome.storage.sync.get([], function() {
   loadAllTimeEntries(renderTimeEntries, showError);
   loadAllProjectsAndActivities(renderProjectTimerButtons, showError);
